7d4067b50fe01c0090b53d0d10f17a94ba05a07f
unknown <tpc3yw@virginia.edu>
2020-08-18 14:43:10 -0400

    static files

--- SolarTax/SolarTax/settings.py
+++ SolarTax/SolarTax/settings.py
@@ -142,7 +142,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 try:
