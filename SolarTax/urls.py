df22221227caf6a950906bc07dd30b9f895d93c9
unknown <tpc3yw@virginia.edu>
2020-08-24 14:05:21 -0400

    Responsive Graph:

--- SolarTax/urls.py
+++ SolarTax/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('models.urls')),
+    path('django_plotly_dash/', include('django_plotly_dash.urls')),
     path('admin/', admin.site.urls),
 ]
--- models/urls.py
+++ models/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import NewSimulationView
-
+# from models.dash_apps.finished_apps import *
 from . import views
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('form/', NewSimulationView.as_view(), name='form'),
     path('dash/', views.dash, name='dash'),
     path('locality-list/', views.IndexView.as_view(), name='locality-list'),
-    path('formtext/', views.request_page, name='test')
+    path('formtext/', views.request_page, name='test'),
+    path('chart/', views.chart, name="chart")
 ]
