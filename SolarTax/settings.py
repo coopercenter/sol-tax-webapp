da39cb87cbd234c0671b8c8d5a6257bbd51524c9
unknown <tpc3yw@virginia.edu>
2020-11-19 11:54:23 -0500

    redo settings

--- SolarTax/settings.py
+++ SolarTax/settings.py
@@ -177,6 +177,13 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+LOGIN_REDIRECT_URL = 'localityName'
+
+EMAIL_HOST = os.environ.get('MAILGUN_SMTP_SERVER', '')
+EMAIL_PORT = os.environ.get('MAILGUN_SMTP_PORT', '')
+EMAIL_HOST_USER = os.environ.get('MAILGUN_SMTP_LOGIN', '')
+EMAIL_HOST_PASSWORD = os.environ.get('MAILGUN_SMTP_PASSWORD', '')
+
 try:
     # Configure Django App for Heroku.
     import django_heroku
@@ -188,16 +195,3 @@ except ImportError:
 #Username: Locality Name
 #Password: admin1213
 
-LOGIN_REDIRECT_URL = 'localityName'
-
-EMAIL_HOST = os.environ.get('MAILGUN_SMTP_SERVER', '')
-EMAIL_PORT = os.environ.get('MAILGUN_SMTP_PORT', '')
-EMAIL_HOST_USER = os.environ.get('MAILGUN_SMTP_LOGIN', '')
-EMAIL_HOST_PASSWORD = os.environ.get('MAILGUN_SMTP_PASSWORD', '')
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'soltaxcoopercenter@gmail.com'
-# EMAIL_HOST_PASSWORD = '3greenApples!'
