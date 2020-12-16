a5cdb21dac6a6b6e0874c9b5f96434e6f882b7f3
unknown <tpc3yw@virginia.edu>
2020-12-16 12:10:29 -0500

    email change

--- SolarTax/settings.py
+++ SolarTax/settings.py
@@ -188,7 +188,7 @@ LOGIN_REDIRECT_URL = 'localityName'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'soltaxcoppercenter@gmail.com'
+EMAIL_HOST_USER = 'soltaxcoopercenter@gmail.com'
 EMAIL_HOST_PASSWORD = '3greenApples!'
 
 
