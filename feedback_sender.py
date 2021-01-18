a0c4a60adaba471961f63047f59314083e48f7f6
unknown <tpc3yw@virginia.edu>
2021-01-18 14:10:42 -0500

    testing

--- feedback_sender.py
+++ feedback_sender.py
@@ -1,27 +1,39 @@
 # Package Scheduler.
 import os
-
 from django.core.asgi import get_asgi_application
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SolarTax.settings')
-
 application = get_asgi_application()
-
-import django
-# import os
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SolarTax.settings')
 django.setup()
-# import django
-# django.setup()
 
-from apscheduler.schedulers.background import BackgroundScheduler
-from feedback_worker import print_update
+from apscheduler.schedulers.blocking import BlockingScheduler
+from models.models import Feedback
+from datetime import *
+from django.core.mail import send_mail
+import time
 
-scheduler = BackgroundScheduler()
+scheduler = BlockingScheduler()
 
-scheduler.add_job(print_update, 'cron', day_of_week='mon-fri', hour=14, minute=7, second=0)
+@scheduler.scheduled_job('cron', day_of_week='mon-fri', hour=14, minute=14)
+def print_update():
+    if(datetime.now().isoweekday() == 1):
+        test = Feedback.objects.filter(date__lte=(datetime.now())).filter(date__gt=(datetime.now() - timedelta(days=3)))
+    else:
+        test = Feedback.objects.filter(date__lte=(datetime.now())).filter(date__gt=(datetime.now() - timedelta(days=1)))
+    print(test)
+
+    if test:
+        message = ""
+        for item in test:
+            message += "From: " + str(item.email) + "\n" + "Date/Time: " + str(item.date) + "\n" + item.message + "\n\n"
+
+        #send_mail('Feedback from ' + str(date.today()), message, 'coopercentersoltax@gmail.com', ['tpc3yw@virginia.edu', 'emm2t@virginia.edu', 'carrie.hearne@dmme.virginia.gov'], fail_silently=False,)
+        send_mail('Feedback from ' + str(date.today()), message, 'coopercentersoltax@gmail.com', ['tpc3yw@virginia.edu'], fail_silently=False,)
+        print("sent_mail")
+
+
+# scheduler.add_job(print_update, 'cron', day_of_week='mon-fri', hour=14, minute=7, second=0)
 
 scheduler.start()
 
-
-    #scheduler.add_job(print_update, 'cron', day_of_week='mon-fri', hour=datetime.now().hour, minute=datetime.now().minute, second=datetime.now().second + 5)
